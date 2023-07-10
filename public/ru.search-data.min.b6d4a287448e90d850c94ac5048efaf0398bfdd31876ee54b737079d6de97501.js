0956b2860146630e70b62748e2b964cdfab59513
Mingyu Huang <2510648h@student.gla.ac.uk>
2023-07-10 10:37:23 +0800

    Added repository .io as a submodule

--- public/ru.search-data.min.b6d4a287448e90d850c94ac5048efaf0398bfdd31876ee54b737079d6de97501.js
+++
@@ -1,1 +0,0 @@
-"use strict";(function(){const e={split:/[^a-zа-яё0-9\w]/gi};e.doc={id:"id",field:["title","content"],store:["title","href","section"]};const t=FlexSearch.create("balance",e);window.bookSearchIndex=t})()
